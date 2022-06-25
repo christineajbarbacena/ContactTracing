947dfe47bae81de8e2cfdeb7163304ae11a2cac0
christineajbarbacena <christinebarbacena9@gmail.com>
2022-06-25 20:44:30 +0800

    Instruction, Date and time Added

--- ContactTracingApp/Form1.Designer.cs
+++ ContactTracingApp/Form1.Designer.cs
@@ -28,29 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtboxDate = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtboxTime = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(268, 9);
+            this.lblTitle.Location = new System.Drawing.Point(243, 9);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(209, 24);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Contact Tracing Form";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.BackColor = System.Drawing.Color.Transparent;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(13, 58);
+            this.lblInstructions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(379, 16);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Kindly provide the needed information accurately and honestly:";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblDate.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(12, 98);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(44, 15);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date:";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtboxDate
+            // 
+            this.txtboxDate.Location = new System.Drawing.Point(62, 91);
+            this.txtboxDate.Name = "txtboxDate";
+            this.txtboxDate.Size = new System.Drawing.Size(211, 22);
+            this.txtboxDate.TabIndex = 3;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(309, 98);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(43, 15);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Time:";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtboxTime
+            // 
+            this.txtboxTime.Location = new System.Drawing.Point(358, 91);
+            this.txtboxTime.Name = "txtboxTime";
+            this.txtboxTime.Size = new System.Drawing.Size(117, 22);
+            this.txtboxTime.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 488);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(716, 475);
+            this.Controls.Add(this.txtboxTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtboxDate);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblInstructions);
             this.Controls.Add(this.lblTitle);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
+            this.Opacity = 0.9D;
             this.Text = "Contact Tracing";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -60,6 +129,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtboxDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtboxTime;
     }
 }
 
