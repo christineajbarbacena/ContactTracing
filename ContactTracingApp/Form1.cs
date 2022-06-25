3a10589c55828bf47573c2322abc9532d09c931f
christineajbarbacena <christinebarbacena9@gmail.com>
2022-06-25 22:51:57 +0800

    File. Writeline completed

--- ContactTracingApp/Form1.cs
+++ ContactTracingApp/Form1.cs
@@ -21,9 +21,23 @@ namespace ContactTracingApp
         private void bttnSubmit_Click(object sender, EventArgs e)
         {
             StreamWriter file = new StreamWriter(@"C:\Users\barbacena\Desktop\ContactTracingResponses\" + txtboxSurname.Text + " " + txtboxFirstName.Text + ".txt", true);
-            file.WriteLine("Date and Time:" + txtboxDate.Text + "," + txtboxTime.Text);
-
-
+            file.WriteLine("Contact Tracing Response");
+            file.WriteLine("Date and Time:" + txtboxDate.Text + " ," + txtboxTime.Text);
+            file.WriteLine("Name:" + txtboxSurname.Text + "," + txtboxFirstName.Text + " " + txtboxMiddleInitial.Text + "." + " " + txtboxSuffix.Text);
+            file.WriteLine("Birth Date: " + txtboxBirthdate.Text);
+            file.WriteLine("Age: " + txtboxAge.Text + " years old");
+            file.WriteLine("Sex:" + txtboxSex.Text);
+            file.WriteLine("Address");
+            file.WriteLine("Province:" + txtboxProvince.Text);
+            file.WriteLine("City:" + txtboxCity.Text);
+            file.WriteLine("Street Name:" + txtboxStreetName.Text);
+            file.WriteLine("Barangay:" + txtboxBarangay.Text);
+            file.WriteLine("Vaccination Information");
+            file.WriteLine("Vaccinated:" + txtboxAYV.Text);
+            file.WriteLine("Booster Shot:" + txtboxBooster.Text);
+            file.WriteLine("1st Dose:" + txtboxFirstDose.Text);
+            file.WriteLine("2nd Dose:" + txtbox2ndDose.Text);
+            file.WriteLine("----End of Response----");
             file.Close();
 
 
