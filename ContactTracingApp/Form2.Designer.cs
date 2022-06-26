9b2687e53e434f8ac02151b4657d2e30ec124254
christineajbarbacena <christinebarbacena9@gmail.com>
2022-06-26 15:14:25 +0800

    Search Button is working

--- ContactTracingApp/Form2.Designer.cs
+++ ContactTracingApp/Form2.Designer.cs
@@ -28,11 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bttnSearch = new System.Windows.Forms.Button();
+            this.lblInstruction1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxDate = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttnSearch
+            // 
+            this.bttnSearch.Location = new System.Drawing.Point(250, 66);
+            this.bttnSearch.Name = "bttnSearch";
+            this.bttnSearch.Size = new System.Drawing.Size(75, 23);
+            this.bttnSearch.TabIndex = 0;
+            this.bttnSearch.Text = "Search";
+            this.bttnSearch.UseVisualStyleBackColor = true;
+            this.bttnSearch.Click += new System.EventHandler(this.bttnAlldata_Click);
+            // 
+            // lblInstruction1
+            // 
+            this.lblInstruction1.AutoSize = true;
+            this.lblInstruction1.Location = new System.Drawing.Point(12, 38);
+            this.lblInstruction1.Name = "lblInstruction1";
+            this.lblInstruction1.Size = new System.Drawing.Size(204, 13);
+            this.lblInstruction1.TabIndex = 1;
+            this.lblInstruction1.Text = "Show all the responses by the given date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // txtboxDate
+            // 
+            this.txtboxDate.Location = new System.Drawing.Point(51, 68);
+            this.txtboxDate.Name = "txtboxDate";
+            this.txtboxDate.Size = new System.Drawing.Size(176, 20);
+            this.txtboxDate.TabIndex = 3;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(12, 75);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(33, 13);
+            this.lblDate.TabIndex = 4;
+            this.lblDate.Text = "Date:";
+            this.lblDate.Click += new System.EventHandler(this.lblDate_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(357, 321);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.txtboxDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblInstruction1);
+            this.Controls.Add(this.bttnSearch);
+            this.Name = "Form2";
+            this.Text = "AdminForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bttnSearch;
+        private System.Windows.Forms.Label lblInstruction1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxDate;
+        private System.Windows.Forms.Label lblDate;
     }
 }
--- ContactTracingApp/Form2.cs
+++ ContactTracingApp/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,31 @@ namespace ContactTracingApp
         {
             InitializeComponent();
         }
+
+        private void bttnAlldata_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader responses = new StreamReader(@"C:\Users\barbacena\Desktop\ContactTracingResponses\" + txtboxDate + "," + "responses.txt");
+                MessageBox.Show(responses.ReadToEnd());
+                responses.Close();
+
+            }//end of try
+            
+            catch
+            {
+                MessageBox.Show("No data on date " + txtboxDate.Text);
+
+            }//end of catch
+
+            
+     
+        }
+
+        private void lblDate_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
