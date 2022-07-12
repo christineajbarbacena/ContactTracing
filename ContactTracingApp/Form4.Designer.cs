50f02b4cb3d341a097caf69dd74c1e8785038776
christineajbarbacena <christinebarbacena9@gmail.com>
2022-07-13 01:13:25 +0800

    Last commit ngayong gabi

--- ContactTracingApp/Form4.Designer.cs
+++ ContactTracingApp/Form4.Designer.cs
@@ -28,8 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.cbcamera = new System.Windows.Forms.ComboBox();
+            this.bttnstart = new System.Windows.Forms.Button();
+            this.pbImageScan = new System.Windows.Forms.PictureBox();
+            this.txtboxScanQr = new System.Windows.Forms.TextBox();
+            this.lblCamera = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageScan)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cbcamera
+            // 
+            this.cbcamera.FormattingEnabled = true;
+            this.cbcamera.Location = new System.Drawing.Point(114, 15);
+            this.cbcamera.Name = "cbcamera";
+            this.cbcamera.Size = new System.Drawing.Size(207, 21);
+            this.cbcamera.TabIndex = 0;
+            // 
+            // bttnstart
+            // 
+            this.bttnstart.Location = new System.Drawing.Point(312, 251);
+            this.bttnstart.Name = "bttnstart";
+            this.bttnstart.Size = new System.Drawing.Size(75, 23);
+            this.bttnstart.TabIndex = 1;
+            this.bttnstart.Text = "Scan";
+            this.bttnstart.UseVisualStyleBackColor = true;
+            this.bttnstart.Click += new System.EventHandler(this.bttnstart_Click);
+            // 
+            // pbImageScan
+            // 
+            this.pbImageScan.Location = new System.Drawing.Point(25, 65);
+            this.pbImageScan.Name = "pbImageScan";
+            this.pbImageScan.Size = new System.Drawing.Size(226, 209);
+            this.pbImageScan.TabIndex = 2;
+            this.pbImageScan.TabStop = false;
+            this.pbImageScan.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtboxScanQr
+            // 
+            this.txtboxScanQr.Location = new System.Drawing.Point(269, 65);
+            this.txtboxScanQr.Multiline = true;
+            this.txtboxScanQr.Name = "txtboxScanQr";
+            this.txtboxScanQr.Size = new System.Drawing.Size(161, 158);
+            this.txtboxScanQr.TabIndex = 3;
+            // 
+            // lblCamera
+            // 
+            this.lblCamera.AutoSize = true;
+            this.lblCamera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCamera.Location = new System.Drawing.Point(49, 20);
+            this.lblCamera.Name = "lblCamera";
+            this.lblCamera.Size = new System.Drawing.Size(59, 16);
+            this.lblCamera.TabIndex = 4;
+            this.lblCamera.Text = "Camera:";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Form4
             // 
@@ -38,12 +96,27 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(442, 311);
+            this.Controls.Add(this.lblCamera);
+            this.Controls.Add(this.txtboxScanQr);
+            this.Controls.Add(this.pbImageScan);
+            this.Controls.Add(this.bttnstart);
+            this.Controls.Add(this.cbcamera);
             this.Name = "Form4";
             this.Text = "Qr Scan Form";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageScan)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbcamera;
+        private System.Windows.Forms.Button bttnstart;
+        private System.Windows.Forms.PictureBox pbImageScan;
+        private System.Windows.Forms.TextBox txtboxScanQr;
+        private System.Windows.Forms.Label lblCamera;
+        private System.Windows.Forms.Timer timer;
     }
 }
--- ContactTracingApp/Form4.cs
+++ ContactTracingApp/Form4.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IronBarCode;
+using AForge.Video;
+using AForge;
+using System.IO;
+using AForge.Video.DirectShow;
+using IronBarCode.Logging;
+
 
 namespace ContactTracingApp
 {
@@ -16,5 +23,48 @@ namespace ContactTracingApp
         {
             InitializeComponent();
         }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice devicecapture;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filterInfoCollection)
+                cbcamera.Items.Add(device.Name);
+            cbcamera.SelectedIndex = 0;
+        }
+
+        private void bttnstart_Click(object sender, EventArgs e)
+        {
+            devicecapture = new VideoCaptureDevice(filterInfoCollection[cbcamera.SelectedIndex].MonikerString);
+            devicecapture.NewFrame += devicecapture_NewFrame;
+            devicecapture.Start();
+
+        }
+
+        private void devicecapture_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+            pbImageScan.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            if (pbImageScan.Image != null)
+
+            {
+
+            }
+        }//end of timer
+
+     
     }
 }
